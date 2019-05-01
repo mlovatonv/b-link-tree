ccc4a00ef460e9ac86bcc5de30e5fe09ce3feaa0
Maria <mlovatonv@gmail.com>
2019-05-01 13:59:31 -0500

    Refactor b-link-node

--- b-link-node.hpp
+++ b-link-node.hpp
@@ -1,10 +1,10 @@
 #include <iostream>
-template<class T>
+template <class T>
 struct NodeTuple
 {
     T value;
-    NodeTuple<T>* next;
-    NodeTuple<T>* AbajoIzq;
+    NodeTuple<T> *next;
+    NodeTuple<T> *AbajoIzq;
 
     NodeTuple(T value){
         next=nullptr;
@@ -17,49 +17,59 @@ template <class T>
 class BLinkNode
 {
     int n;
-    BLinkNode<T> * right;
-    NodeTuple<T> * start;
+    BLinkNode<T> *right;
+    NodeTuple<T> *start;
 public:
-    BLinkNode(){
-        this->start=nullptr;
+    BLinkNode()
+    {
+        this->start = nullptr;
         n=0;
     };
 
-    void insert(T value){
-        NodeTuple<T> *aux_ins= new NodeTuple<T>(value);
-        if(start==nullptr){
-            this->start=aux_ins;
+    void insert(T value)
+    {
+        NodeTuple<T> *aux_ins = new NodeTuple<T>(value);
+        if (start == nullptr)
+        {
+            this->start = aux_ins;
         } 
         
         else{
-            NodeTuple<T>* aux= start;
-            if(value < start->value){
-                start=aux_ins;
-                aux_ins->next=aux;
+            NodeTuple<T> *aux = start;
+            if (value < start->value)
+            {
+                start = aux_ins;
+                aux_ins->next = aux;
             }
             else{
-                while(aux->next != nullptr && aux->next->value<value){
-                    aux=aux->next;
+                while (aux->next != nullptr && aux->next->value < value)
+                {
+                    aux = aux->next;
                 }
-                if(aux->next!=nullptr){
-                    NodeTuple<T>* aux2;
-                    aux2=aux->next;
-                    aux->next=aux_ins;
-                    aux_ins->next=aux2;
+                if (aux->next != nullptr)
+                {
+                    NodeTuple<T> *aux2;
+                    aux2 = aux->next;
+                    aux->next = aux_ins;
+                    aux_ins->next = aux2;
                 }
-                else{
-                    aux->next=aux_ins;
-                    aux_ins->next=nullptr;
+                else
+                {
+                    aux->next = aux_ins;
+                    aux_ins->next = nullptr;
                 }
             }
         }
         
     };
-    void Print(){
-        NodeTuple<T>* aux= start;
-        while(aux!=nullptr){
-            std::cout<<aux->value<<"\n";
-            aux=aux->next;
+
+    void print()
+    {
+        NodeTuple<T> *aux = start;
+        while (aux != nullptr)
+        {
+            std::cout << aux->value << "\n";
+            aux = aux->next;
         }
     };
 };
