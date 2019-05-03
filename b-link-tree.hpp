dc2c928ef92ffd9f19adddc57a97c2cc9ec2a50f
Maria <mlovatonv@gmail.com>
2019-05-03 13:57:13 -0500

    First steps of insert method

--- b-link-tree.hpp
+++ b-link-tree.hpp
@@ -3,8 +3,9 @@
 template <class KeyType, class DataType>
 class BLinkTree
 {
+public: 
     BLinkNode<KeyType, DataType> *root;
-public:   
+  
     BLinkTree()
     {
         this->root = new BLinkNode<KeyType, DataType>(LEAF);
@@ -20,8 +21,21 @@ public:
         aux_node->insert(key, data);
     };
 
-    void split_leaf()
-    {};
+    void split_leaf(BLinkNode<KeyType, DataType> *original_node)
+    {
+        NodeTuple<KeyType, DataType> *middle_tuple = original_node->divide();
+        BLinkNode<KeyType, DataType> *parent_node = new BLinkNode<KeyType, DataType>(NON_LEAF);
+        BLinkNode<KeyType, DataType> *next_node = new BLinkNode<KeyType, DataType>(LEAF);
+
+        next_node->start = middle_tuple;
+        parent_node->start = new NodeTuple<KeyType, DataType>(middle_tuple->value);
+        parent_node->start->left_node = original_node;
+        parent_node->start->next = new NodeTuple<KeyType, DataType>(middle_tuple->get_last()->value);
+        parent_node->start->next->left_node = next_node;
+        original_node->next_node = next_node;
+
+        this->root = parent_node;
+    };
 
     void split_non_leaf()
     {   
