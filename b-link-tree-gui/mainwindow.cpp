7278dfd303961fd68d1714d91869d14c5fe54360
Maria <mlovatonv@gmail.com>
2019-05-16 18:50:46 -0500

    Improve GUI

--- b-link-tree-gui/mainwindow.cpp
+++ b-link-tree-gui/mainwindow.cpp
@@ -12,3 +12,9 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->resultText->setText(ui->searchText->toPlainText());
+    ui->searchText->setText("");
+}
